0aa5d16798654248c7ad8275ed628e0810217148
ozer <337706@student.mboutrecht.nl>
2025-11-05 14:50:32 +0100

    Fix GitHub Pages deployment: add base for Vite, HashRouter

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/Pokemin/', // this must match your repo name
+  base: '/Pokemin/', // dit moet exact je repo-naam zijn
   plugins: [react()],
 });
